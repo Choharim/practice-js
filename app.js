8db6298b43f7677bbb7a411c8777e25d7f4cf6b1
Choharim <jhl8586@naver.com>
2020-11-23 14:44:54 +0900

    Add filterTodoFn

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-
+const filterOption = document.querySelector(".filter-todo");
 
 todoButton.addEventListener("click",addTodo);
 todoList.addEventListener("click",deleteCheck);
-
+filterOption.addEventListener("click",filterTodo);
 
 function addTodo(event){
   event.preventDefault();
@@ -44,4 +44,29 @@ function deleteCheck(event){
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(event){
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo){
+    switch(event.target.value){
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if(todo.classList.contains("completed")){
+          todo.style.display = "flex";
+        }else{
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if(!todo.classList.contains("completed")){
+          todo.style.display = "flex";
+        }else{
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
